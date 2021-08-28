627857af501345b2c5bb2c93f7066d5e57c33b5d
Aki-vito <abdullahkefahi@gmail.com>
2021-08-28 16:41:30 +0300

    Movement + ragdoll

--- player.gd
+++ player.gd
@@ -1,9 +1,21 @@
 extends KinematicBody2D
+export (int) var speed = 100
+var velo = Vector2()
 
 onready var anim = $AnimatedSprite
 
-func _physics_process(delta):
+func get_input():
+	velo = Vector2()
+	if Input.is_action_pressed("right"):
+		velo.x += 1
+	if Input.is_action_pressed("left"):
+		velo.x -= 1
+	velo = velo.normalized() * speed
+			
 
+func _physics_process(delta):
+	get_input()
+	velo = move_and_slide(velo)
 	var axisX = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var hito = Input.is_action_pressed("space")
 	if hito == true:
