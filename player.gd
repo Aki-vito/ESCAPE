d6abf4e6d335adc0799651e59ff666231f279aeb
Aki-vito <abdullahkefahi@gmail.com>
2021-08-28 16:30:36 +0300

    added attack, long press space

--- player.gd
+++ player.gd
@@ -3,13 +3,16 @@ extends KinematicBody2D
 onready var anim = $AnimatedSprite
 
 func _physics_process(delta):
+
 	var axisX = Input.get_action_strength("right") - Input.get_action_strength("left")
+	var hito = Input.is_action_pressed("space")
+	if hito == true:
+		anim.animation = "attack" 
 	if axisX > 0:
 		anim.animation = "run"
 		anim.flip_h = false
 	elif axisX < 0:
 		anim.animation = "run"
 		anim.flip_h = true	
-	else:
-		anim.animation = "idle"
-		
+	elif axisX == 0 and hito == false:
+		anim.animation = "idle"		
