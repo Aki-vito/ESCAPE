060ef8a7951c5039c42572a3564beebdfd5b3284
Aki-vito <abdullahkefahi@gmail.com>
2021-08-29 16:43:47 +0300

    added all animations

--- player.gd
+++ player.gd
@@ -2,52 +2,52 @@ extends KinematicBody2D
 export (int) var speed = 100
 var velo = Vector2()
 onready var anim = $AnimatedSprite
-var tru = true 
+const gravity = 1000
 var entered = false
 onready var zozo = get_node("/root/Node2D/zozo")
 var hito = false
-var jumpo = false
-var pos = Vector2()
+var hito2 = false
+var hito3 = false
 func get_input():
 	var axisa = Input.get_action_strength("right") - Input.get_action_strength("left")
 	velo = Vector2()
-	
-	if Input.is_action_just_pressed("space"):
-		jumpo = true
-		anim.animation = "jump"
-		yield(get_tree().create_timer(1), "timeout")
-		jumpo = false
-	
-	elif Input.is_action_pressed("right"):
+	if Input.is_action_pressed("right"):
 		velo.x += 1
 	elif Input.is_action_pressed("left"):
 		velo.x -= 1
-		
 	elif Input.is_action_pressed("control"):
 		hito = true
-		anim.animation = "attack" 
-		yield(get_tree().create_timer(2), "timeout")
+		anim.animation = "attack1" 
+		yield(get_tree().create_timer(0.7), "timeout")
 		hito = false
+	elif Input.is_action_just_pressed("space"):
+		hito2 = true
+		anim.animation = "attack2"
+		yield(get_tree().create_timer(0.6), "timeout")
+		hito2 = false
+	elif Input.is_action_just_pressed("shift"):
+		hito3 = true
+		anim.animation = "attack3"
+		yield(get_tree().create_timer(0.8), "timeout")
+		hito3 = false
+		
 	velo = velo.normalized() * speed
 
 func _physics_process(delta):
-	get_input()
-	pos = Vector2()
-	velo = move_and_slide(velo)
-
 	var axisX = Input.get_action_strength("right") - Input.get_action_strength("left")
-	if axisX > 0 and hito == false:
+	get_input()
+	velo = move_and_slide(velo)
+	if axisX > 0 and hito == false and hito2 == false and hito3 == false:
 		anim.animation = "run"
 		anim.flip_h = false
-	elif axisX < 0 and hito == false:
+	elif axisX < 0 and hito == false and hito2 == false and hito3 == false:
 		anim.animation = "run"
 		anim.flip_h = true	
-	elif axisX == 0 and hito == false and jumpo == false:
+	elif axisX == 0 and hito == false and hito2 == false and hito3 == false:
 		anim.animation = "idle"
-	elif axisX == 0 and hito == false and jumpo == true:
-		self.position.x += 1.5
+	 
 	
-
+	
 	_dies()
 
 func _on_Object_body_entered(body):
